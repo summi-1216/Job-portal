52613eb645124127307ffade94e963df3ce9a583
Summi Sinha <Summisinha1198@gmail.com>
2024-08-28 00:06:11 +0530

    initial commit

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { Button } from './ui/button'
+import { Search } from 'lucide-react'
+import { useDispatch } from 'react-redux';
+import { setSearchedQuery } from '@/redux/jobSlice';
+import { useNavigate } from 'react-router-dom';
+
+const HeroSection = () => {
+    const [query, setQuery] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const searchJobHandler = () => {
+        dispatch(setSearchedQuery(query));
+        navigate("/browse");
+    }
+
+    return (
+        <div className='text-center'>
+            <div className='flex flex-col gap-5 my-10 bg-slate-50'>
+                <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 Job Hunt Website</span>
+                <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#c23886] '>Dream Jobs</span></h1>
+                <p class="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">JobSphere is a dynamic and user-friendly job portal designed to connect job seekers with opportunities across various industries and locations. The platform serves as a central hub where employers and job seekers converge, offering a comprehensive range of features that streamline the job search and recruitment process.</p>
+                <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
+                    <input
+                        type="text"
+                        placeholder='Find your dream jobs'
+                        onChange={(e) => setQuery(e.target.value)}
+                        className='outline-none border-none w-full'
+
+                    />
+                    <Button onClick={searchJobHandler} className="rounded-r-full bg-[#c23886] hover:bg-[#ae3278]">
+                        <Search className='h-5 w-5' />
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
